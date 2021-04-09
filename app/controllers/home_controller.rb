7fe9d9c52ff713f4af0dd4e947eeffeb6e776b3f
Ben Hodges <thebenhodges@gmail.com>
2021-04-09 18:37:51 +0100

    contact page changed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,11 +6,6 @@ class HomeController < ApplicationController
   end
 
   def request_contact
-    name = params[:name]
-    email = params[:email]
-    telephone = params[:telephone]
-    message = params[:message]
-    ContactMailer.contact_email(email, name, telephone, message).deliver_now
     flash[:notice] = I18n.t('home.request_contact.email_sent')
     redirect_to root_path
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :games
   devise_for :users
   root 'home#home'
   get 'contact', to: 'home#contact'
