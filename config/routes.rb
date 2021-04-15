b567b6a16fe4cbf96ec8aa932abb2e36a95afce9
James Head <james.head@mac.com>
2021-04-15 18:47:04 +0100

    Added the download files to routes.rb, controllers/games_controller.rb, view/games/games.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'contact', to: 'home#contact'
   get 'games', to: 'games#games'
   get 'about', to: 'home#about'
+  get 'download_game_windows', to: 'game_download_game_windows'
+  get 'download_game_mac', to: 'game_download_game_mac'
   post 'request_contact', to: 'home#request_contact'
   mount SimpleDiscussion::Engine => "/forum"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
