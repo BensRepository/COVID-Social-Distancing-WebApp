ab93aedc6afc9d16b24fc0dec3c4062c44b52c00
james-head <james.head@mac.com>
2021-04-16 11:00:44 -0700

    Got rid of the prevouis changes as they didn't work. Implemented the game download into the game page. The changes have been added to the games.html.haml file and en.yml file.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,3 @@
 class GamesController < ApplicationController
   def games
   end
-
-  def download_game_windows
-    send_file "#{Rails.root}public/PersonalSpaceInvaders(windows).zip", type: "application/zip", x_sendfile: true
-  end
-
-  def download_game_mac
-    send_file "#{Rails.root}public/PersonalSpaceInvaders(mac).zip", type: "application/zip", x_sendfile: true
-  end
