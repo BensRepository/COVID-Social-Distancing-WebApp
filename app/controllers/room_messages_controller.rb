025c02a912fd5d046d615469fa66a2a2524290ed
Andrei Ciprian Alexandru <34582885+Ciucky@users.noreply.github.com>
2021-05-05 16:26:30 +0300

    Update room_messages_controller.rb

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -5,9 +5,10 @@ class RoomMessagesController < ApplicationController
     @room_message = RoomMessage.create user: current_user,
                                        room: @room,
                                        message: params.dig(:room_message, :message)
-   head :ok
     
    RoomChannel.broadcast_to @room, @room_message
+    
+   head :ok
   end
 
   protected
