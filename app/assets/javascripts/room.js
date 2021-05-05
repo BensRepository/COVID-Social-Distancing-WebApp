4394003824d0906fb16e2a7f83fb4dad477bcb3c
Andrei Ciprian Alexandru <34582885+Ciucky@users.noreply.github.com>
2021-05-05 13:32:17 +0300

    Update room.js

--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).on('turbolinks:load', function() {
   $('#new_room_message').on('ajax:success', function(a, b,c ) {
     $(this).find('input[type="text"]').val('');
   });
