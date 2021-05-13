e23b79807f013a62158f62ced9b844d179f62ffd
bh00527 <62959828+bh00527@users.noreply.github.com>
2021-05-13 01:08:48 +0100

    Update games.js

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -42,8 +42,7 @@ function test()
   }
 }};
 
-   //      $( document ).ready(function(){
-   //        var score = 0;
-   //        test();
-   //
-   // });
+$( document ).ready(function(){
+var score = 0;
+test();
+   });
